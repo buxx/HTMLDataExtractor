2320c375ef352bff993330ec8207139d7d63738b
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-07-01 10:03:36 +0200

    Refactorise dalz sandbox

--- sandbox/dalz/data.py
+++ sandbox/dalz/data.py
@@ -1,29 +1,44 @@
-import re
-from pyquery import PyQuery
-from sandbox.dalz.match import ArticleFileContentMatch
+from sandbox.dalz.match import ArticleFileContentMatch, ArticleAndCommentsFileContentMatch
 from tde.FileData import FileData
 from tde.FilesData import FilesData
 from tde.HTMLData import HTMLData
 
+# Selecteurs CSS
+selector_title = 'h1.entry-title'
+selector_comment = 'a.cmnt_count'
+selector_article_author = 'div.blogauthor div.left'
+
+# Pattern d'expressions régulières
+pattern_comment_count = '^([0-9]+) Commentaire[s]*'
+pattern_article_publication_date = '^De\ ([a-zA-Z0-9?]+), le ([0-9]{2}\.[0-9]{2}\.[0-9]{4}) à ([0-9]{2}:[0-9]{2})'
+pattern_article_author = '^De\ ([a-zA-Z0-9?]+),'
 
 class ArticleFileData(FileData, HTMLData):
     _match_class = ArticleFileContentMatch
 
-    def _get_data_for_text(self, text):
-        raise NotImplementedError()
+    def _get_text_data_name(self, text):
+        return self._extract_text(text, selector_title)
+
+
+class ArticleFilesData(FilesData, HTMLData):
+    _match_class = ArticleFileContentMatch
 
     def _get_text_data_name(self, text):
-        return self._extract_text(text, 'h1.entry-title')
+        return self._extract_text(text, selector_title)
 
 
-class ArticleCommentCountFileData(ArticleFileData):
+class ArticleAndCommentsFileData(ArticleFileData):
+    _match_class = ArticleAndCommentsFileContentMatch
+
+
+class ArticleCommentCountFileData(ArticleAndCommentsFileData):
     _name = 'Comments_count_by_article'
     _key_name = 'Article name'
     _value_name = 'Comments count'
 
     def _get_data_for_text(self, text):
-        comments_count = self._extract_text(text, 'a.cmnt_count')
-        return re.search('^([0-9]+) Commentaires', comments_count).group(1)
+        comments_count = self._extract_text(text, selector_comment)
+        return self._re_extract_text(comments_count, pattern_comment_count)
 
 
 class ArticlePublicationDateFileData(ArticleFileData):
@@ -32,10 +47,9 @@ class ArticlePublicationDateFileData(ArticleFileData):
     _value_name = 'Publication date'
 
     def _get_data_for_text(self, text):
-        blog_author = self._extract_text(text, 'div.blogauthor div.left')
-        pattern = '^De\ ([a-zA-Z0-9]+), le ([0-9]{2}\.[0-9]{2}\.[0-9]{4}) à ([0-9]{2}:[0-9]{2})'
-        extract = re.search(pattern, blog_author)
-        return "%s %s" % (extract.group(2), extract.group(3))
+        article_author = self._extract_text(text, selector_article_author)
+        pattern = pattern_article_publication_date
+        return self._re_extract_text(article_author, pattern, (2, 3), '%s %s')
 
 
 class ArticleAuthorFileData(ArticleFileData):
@@ -44,18 +58,18 @@ class ArticleAuthorFileData(ArticleFileData):
     _value_name = 'Author name'
 
     def _get_data_for_text(self, text):
-        blog_author = self._extract_text(text, 'div.blogauthor div.left')
-        return re.search('^De\ ([a-zA-Z0-9]+),', blog_author).group(1)
+        article_author = self._extract_text(text, selector_article_author)
+        return self._re_extract_text(article_author, pattern_article_author)
 
 
-class AuthorArticleCountFilesData(ArticleFileData):
+class AuthorArticleCountFilesData(ArticleFilesData):
     _name = 'Author_articles_count'
     _key_name = 'Author name'
     _value_name = 'Articles count'
 
     def _get_text_data_name(self, text):
-        blog_author = self._extract_text(text, 'div.blogauthor div.left')
-        return re.search('^De\ ([a-zA-Z0-9]+),', blog_author).group(1)
+        article_author = self._extract_text(text, selector_article_author)
+        return self._re_extract_text(article_author, pattern_article_author)
 
     def _get_data_for_text(self, text):
         return 1
--- sandbox/dalz/match.py
+++ sandbox/dalz/match.py
@@ -4,3 +4,7 @@ from tde.HTMLFileContentMatch import HTMLFileContentMatch
 class ArticleFileContentMatch(HTMLFileContentMatch):
     def match(self):
         return bool(len(self._parser('div.blogitem')) == 1)
+
+class ArticleAndCommentsFileContentMatch(HTMLFileContentMatch):
+    def match(self):
+        return len(self._parser('div.blogitem')) == 1 and len(self._parser('a.cmnt_count'))
