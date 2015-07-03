ca35dea248f960822236568d5cbb6d709f8bd5ff
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-07-04 00:20:56 +0200

    modify tests.py to exit with correct state

--- tests.py
+++ tests.py
@@ -4,4 +4,6 @@ test_loader = unittest.defaultTestLoader
 test_runner = unittest.TextTestRunner()
 test_suite = test_loader.discover('tests', 'Test*.py')
 
-test_runner.run(test_suite)
+test_result = test_runner.run(test_suite)
+if test_result.failures or test_result.errors:
+    exit(1)
